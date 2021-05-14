54701b94569e47aa6fc66e8d3507d26fab0f0510
andrecs <12188364+andrecsilva@users.noreply.github.com>
2021-05-14 14:59:05 -0300

    draw.sh now accepts arguments for scale and output format.
    Defaults are 128 for scale and pdf for output formats.

--- draw.sh
+++ draw.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 #When using the -n2 option, neato ignores any kind of scales (-s or inputscale parameter).
 #The following awk script essentially scale the points.
-awk '
+#usage ./draw.sh <scale> <output_format>
+#<scale> the scale of the whole picture.
+#<output_format> the output format of the resulting picture (e.g. pdf, svg, png). Check neato's manual page for more options.
+SCALE=${1:-128}
+OUTPUT=${2:-pdf}
+awk -v scale=$SCALE '
 match($0,/pos="[^"]*"/){
-	scale = 128
 	head = substr($0,0,RSTART-1)
 	tail = substr($0,RSTART,RLENGTH)
 	rest = substr($0,RSTART+RLENGTH)
@@ -17,4 +21,4 @@ match($0,/pos="[^"]*"/){
 	next
 }
 {print}
-' | neato -n2 -Nxlabel="\N" -Nshape=point -Tpdf
+' | neato -n2 -Nxlabel="\N" -Nshape=point -T$OUTPUT
