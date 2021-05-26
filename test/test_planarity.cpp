32a7f8dc1a2be9da4d93676ac1303b8debca061b
andrecs <12188364+andrecsilva@users.noreply.github.com>
2021-05-26 15:18:50 -0300

    Added test for smallest1sidedcycle

--- test/test_planarity.cpp
+++ test/test_planarity.cpp
@@ -138,6 +138,37 @@ auto test_doublePlanarCover()
 	ASSERT(result);
 }
 
+auto test_smalles1sidescycle(){
+	IndexedGraph<AdjList> g {gdraw::getKpq<AdjList>(3,3)};
+	
+	rotations_t<AdjList> rotations = {
+		{edge(0,3,g.getGraph()).first,edge(0,4,g.getGraph()).first,edge(0,5,g.getGraph()).first},
+		{edge(1,5,g.getGraph()).first,edge(1,4,g.getGraph()).first,edge(1,3,g.getGraph()).first},
+		{edge(2,3,g.getGraph()).first,edge(2,5,g.getGraph()).first,edge(2,4,g.getGraph()).first},
+		{edge(3,1,g.getGraph()).first,edge(3,2,g.getGraph()).first,edge(3,0,g.getGraph()).first},
+		{edge(4,0,g.getGraph()).first,edge(4,2,g.getGraph()).first,edge(4,1,g.getGraph()).first},
+		{edge(5,0,g.getGraph()).first,edge(5,2,g.getGraph()).first,edge(5,1,g.getGraph()).first}
+	};
+
+	auto edgei_map = get(boost::edge_index, g.getGraph());
+	std::vector<int> esignals(num_edges(g.getGraph()),1);
+
+	esignals[boost::get(edgei_map,edge(1,4,g.getGraph()).first)] = -1;
+	esignals[boost::get(edgei_map,edge(2,5,g.getGraph()).first)] = -1;
+
+	auto eg = EmbeddedGraph<AdjList>(std::move(g),std::move(rotations),std::move(esignals));
+
+	auto cycle = smallest1SidedCycle(eg);
+
+	//for(auto&& e : cycle)
+	//	std::cout << e << ' ';
+	//std::cout << std::endl;
+
+	ASSERT(cycle.size()==4);
+
+}
+
+
 int main(){
 	std::cout << "Testing : " << __FILE__ << std::endl;
 
@@ -148,4 +179,5 @@ int main(){
 	test_doubleCover();
 	test_doublePlanarCover();
 	test_largestfacialcycle();
+	test_smalles1sidescycle();
 }
