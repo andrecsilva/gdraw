88d8f41fe3c71d1ca255e46c4ea14337791a4eea
andrecs <12188364+andrecsilva@users.noreply.github.com>
2021-06-04 14:09:20 -0300

    Some tests for bridges.

---
+++ test/test_bridges.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cassert>
+
+
+#include <boost/graph/adjacency_list.hpp>
+
+#include <gdraw/graph_types.hpp>
+
+#include <gdraw/generators.hpp>
+#include <gdraw/util.hpp>
+#include <gdraw/planar_graphs.hpp>
+
+#define ASSERT(x) { if (!(x)) std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; }
+
+using AdjList = boost::adjacency_list<
+	boost::vecS
+	,boost::vecS
+	,boost::undirectedS
+	,boost::property<boost::vertex_index_t,size_t>
+	,boost::property<boost::edge_index_t,size_t>
+	>; 
+
+using namespace gdraw;
+
+int main(){
+
+
+	std::cout << "Testing : " << __FILE__ << std::endl;
+}
