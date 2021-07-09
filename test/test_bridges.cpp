38659421726813acd58d2deaf5f33e88150a5fcc
andrecs <12188364+andrecsilva@users.noreply.github.com>
2021-07-09 14:43:56 -0300

    Tests for bridgeOverlap.

--- test/test_bridges.cpp
+++ test/test_bridges.cpp
@@ -22,8 +22,82 @@ using AdjList = boost::adjacency_list<
 
 using namespace gdraw;
 
+auto test_bridgeOverlap(){
+	auto g = IndexedGraph{genCycle<AdjList>(6)};
+
+	std::vector<edge_t<AdjList>> facial_cycle = 
+	{g.edge(0,1).value(),
+	g.edge(1,2).value(),
+	g.edge(2,3).value(),
+	g.edge(3,4).value(),
+	g.edge(4,5).value(),
+	g.edge(5,0).value()
+	};
+
+	std::vector<bool> b1_attachments = {false,true,false,true,false,true};
+	auto b2_attachments = b1_attachments;
+
+	auto overlap = bridgeOverlap(g,b1_attachments,b2_attachments,facial_cycle);
+
+	//for(auto&& v : overlap.value())
+	//	std::cout << v << ' ';
+	//std::cout << std::endl;
+
+	ASSERT(overlap && overlap.value().size()==3);
+}
+
+auto test_bridgeOverlap2(){
+	auto g = IndexedGraph{genCycle<AdjList>(6)};
+
+	std::vector<edge_t<AdjList>> facial_cycle = 
+	{g.edge(0,1).value(),
+	g.edge(1,2).value(),
+	g.edge(2,3).value(),
+	g.edge(3,4).value(),
+	g.edge(4,5).value(),
+	g.edge(5,0).value()
+	};
+
+	std::vector<bool> b1_attachments = {false,true,false,false,true,false};
+	std::vector<bool> b2_attachments = {false,false,true,false,false,true};
+	
+
+	auto overlap = bridgeOverlap(g,b1_attachments,b2_attachments,facial_cycle);
+
+	//for(auto&& v : overlap.value())
+	//	std::cout << v << ' ';
+	//std::cout << std::endl;
+	ASSERT(overlap && overlap.value().size()==4);
+
+}
+
+auto test_bridgeOverlap3(){
+	auto g = IndexedGraph{genCycle<AdjList>(6)};
+
+	std::vector<edge_t<AdjList>> facial_cycle = 
+	{g.edge(0,1).value(),
+	g.edge(1,2).value(),
+	g.edge(2,3).value(),
+	g.edge(3,4).value(),
+	g.edge(4,5).value(),
+	g.edge(5,0).value()
+	};
+
+	std::vector<bool> b1_attachments = {false,true,false,false,true,false};
+	std::vector<bool> b2_attachments = {false,false,true,false,false,false};
+	
+
+	auto overlap = bridgeOverlap(g,b1_attachments,b2_attachments,facial_cycle);
+
+	ASSERT(!overlap);
+
+}
+
+
 int main(){
-
-
 	std::cout << "Testing : " << __FILE__ << std::endl;
+
+	test_bridgeOverlap();
+	test_bridgeOverlap2();
+	test_bridgeOverlap3();
 }
